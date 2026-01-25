a9de68f08ef57b3f3a79dc159b9eaf4d5908da50
corechuv <chuv.ole@icloud.com>
2026-01-25 19:02:29 +0100

    gift

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -1,4 +1,4 @@
-import { ButtonHTMLAttributes, ReactNode } from "react";
+import { type ButtonHTMLAttributes, type ReactNode } from "react";
 import styles from "./Button.module.scss";
 
 type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
